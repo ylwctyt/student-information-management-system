ee25c31b5b9ace98144001e4f830f3a2496a83d5
不蠢会死的某翠 <shadowspacex@163.com>
2017-12-28 02:47:57 +0800

    12.28 changed into django 2.0
    the basic framework of the system

--- user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-urlpatterns = [
-    url(r'^registration/complete/', views.registration_complete, name='user_registration_complete'),
-    url(r'^registration/closed/', views.registration_closed, name='user_registration_closed'),
-    url(r'^registration/', views.registration, name='user_registration'),
-]
