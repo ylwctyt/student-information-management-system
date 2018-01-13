79a3b2f2f7a67eef07ffbc303ecd539d131cf1d7
不蠢会死的某翠 <shadowspacex@163.com>
2018-01-13 22:39:22 +0800

    :building_construction: Change the urls

--- users/urls.py
+++ users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='users.views.index'),
     path('<str:username>/', views.person_index, name='users.views.person_index'),
+    path('<str:username>/profile', views.profile, name='users.views.profile'),
 ]
